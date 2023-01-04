f6690bb553fc0dedca655f5e998102db88058149
rzarroca <ricardozarroca@gmail.com>
2023-01-04 15:51:22 +0000

    Add tailwind and prettier config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+		'./app/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		colors: {
+			black: '#252525',
+			white: 'rgb(226, 232, 240)',
+			gray: 'rgb(148, 163, 184)',
+			primary: 'rgb(251, 191, 36)',
+			secondary: 'rgb(245, 158, 11)',
+		},
+		extend: {},
+	},
+	plugins: [],
 }
