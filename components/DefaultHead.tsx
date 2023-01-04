3ca993dd2969a83f42b427786702a3bc93bd69a0
rzarroca <ricardozarroca@gmail.com>
2023-01-04 15:51:43 +0000

    Add Default Head to all Pages

---
+++ components/DefaultHead.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export const DefaultHead = () => {
+	return (
+		<Head>
+			<title>Calima Holy Cards</title>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+	)
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultHead } from 'components/DefaultHead'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<DefaultHead />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
